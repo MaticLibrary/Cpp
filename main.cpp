d92e32b5b30c5feab19c29d490c357e904f4a407
MaticLibrary <150859422+MaticLibrary@users.noreply.github.com>
2025-03-27 22:19:06 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,14 +1,27 @@
-#include "Wektor.h"
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
 
 int main() {
-    Wektor w3 = {1.5, 2.5, 3.5, 4.5, 5.5};
-    wypisz_wektor(w3);
-
-    Wektor w4(w3);
-    cout << "Wektor w4 (to_string): " << w4.to_string() << endl;
-
-    Wektor w5;
-    wypisz_wektor(w5);
-
+    string zdanie;
+    getline(cin, zdanie);
+    string cyfry = "";
+    string tekst = "";
+    for (char ch : zdanie) {
+        if (isdigit(ch)) {
+            cyfry += ch;
+        } else {
+            tekst += ch;
+        }
+    }
+    int suma = 0;
+    for (char ch : cyfry) {
+        suma += ch - '0';
+    }
+    cout << suma << endl;
+    cout << cyfry << endl;
+    cout << tekst << endl;
     return 0;
 }
